cd8789afc0f574ec88b73869880d50b9d43cd414
Jack Corddry <jackcorddry@gmail.com>
2024-11-23 00:23:39 -0800

    Compilation fixes

--- src/BalancerSwapHelper.sol
+++ src/BalancerSwapHelper.sol
@@ -1,30 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {IVault} from "lib/balancer-v2-monorepo/pkg/interfaces/contracts/vault/IVault.sol";
+import {IVault, IAsset} from "lib/balancer-v2-monorepo/pkg/interfaces/contracts/vault/IVault.sol";
 import {IERC20} from "lib/forge-std/src/interfaces/IERC20.sol";
-import {FixedPointMathLib} from "lib/solmate/src/utils/FixedPointMathLib.sol"
+import {FixedPointMathLib} from "lib/solmate/src/utils/FixedPointMathLib.sol";
 
 contract BalancerSwapHelper {
-    uising FixedPointMathLib for uint256;
+    using FixedPointMathLib for uint256;
 
     address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address constant GHO = 0x40D16FC0246aD3160Ccc09B8D0D3A2cD28aE6C2f;
     address constant POOL = 0x854B004700885A61107B458f11eCC169A019b764;
     IVault constant VAULT = IVault(0xBA12222222228d8Ba445958a75a0704d566BF2C8);
 
-    uint256 constant MAX_SLIPPAGE = .995e18
+    uint256 constant MAX_SLIPPAGE = .995e18;
     
     /// @dev approve first, returns tokens directly to wallet
-    function helpSwap(address wallet, address owner, uint256 amount) public returns (uint256 amountOut) {
+    function helpSwap(address payable wallet, uint256 amount) public returns (uint256 amountOut) {
         IERC20(USDC).transferFrom(wallet, address(this), amount);
 
         IERC20(USDC).approve(address(POOL), amount);
         IVault.SingleSwap memory swap = IVault.SingleSwap({
             poolId: bytes32(abi.encode(POOL)),
             kind: IVault.SwapKind.GIVEN_IN,
-            assetIn: IVault.IAsset(USDC),
-            assetOut: IVault.IAsset(GHO),
+            assetIn: IAsset(USDC),
+            assetOut: IAsset(GHO),
             amount: amount,
             userData: ""
         });
@@ -39,6 +39,6 @@ contract BalancerSwapHelper {
         uint256 limit = amount.mulWadDown(MAX_SLIPPAGE);
         uint256 deadline = type(uint256).max;
 
-        IVault.swap(swap, funds, limit, deadline);
+        return VAULT.swap(swap, funds, limit, deadline);
     }
 }
